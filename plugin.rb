6bfea5e9ca5bae34e1f3a3325a7993b625680825
fethi bey abi ayad <fethi.bey.abi.ayad.ccsmtl@ssss.gouv.qc.ca>
2025-05-05 08:48:14 -0400

    fix admin/users_controller

--- plugin.rb
+++ plugin.rb
@@ -1,46 +1,31 @@
 # name: no-email-export
-# about: Supprime les emails des exports CSV d'utilisateurs
+# about: Remove email from user CSV export
 # version: 0.1
-# authors: TonNom
-# url: https://github.com/ton-org/no-email-user-export
-
-# enabled_site_setting :no_email_user_export_enabled
+# authors: Your Name
 
 after_initialize do
-    module ::NoEmailUserExport
-      class Engine < ::Rails::Engine
-        engine_name "no_email_user_export"
-        isolate_namespace NoEmailUserExport
-      end
-    end
+    require_dependency 'admin/users_controller'
   
-    require_dependency 'admin/users_list_controller'
-  
-    module ::Admin::UsersListControllerExtension
-      def render_csv(users)
-        csv = CSV.generate do |csv_rows|
-          csv_rows << [
-            I18n.t("admin.users.csv.username"),
-            I18n.t("admin.users.csv.name"),
-            I18n.t("admin.users.csv.created_at"),
-            I18n.t("admin.users.csv.last_seen_at"),
-            I18n.t("admin.users.csv.ip_address")
-          ]
-  
-          users.each do |user|
-            csv_rows << [
-              user.username,
-              user.name,
-              user.created_at,
-              user.last_seen_at,
-              user.ip_address
-            ]
+    module ::NoEmailExport
+      module UsersControllerPatch
+        def export_csv
+          users = User.real.includes(:groups)
+          csv = CSV.generate do |csv|
+            csv << ["Username", "Name", "Created At", "Groups"]
+            users.find_each do |user|
+              csv << [
+                user.username,
+                user.name,
+                user.created_at,
+                user.groups.pluck(:name).join(", ")
+              ]
+            end
           end
-        end
   
-        send_data(csv.encode("UTF-8"), type: "text/csv; charset=utf-8; header=present", filename: "users.csv")
+          send_data csv, type: 'text/csv; charset=utf-8; header=present', filename: "users.csv"
+        end
       end
     end
   
-    ::Admin::UsersListController.prepend ::Admin::UsersListControllerExtension
+    ::Admin::UsersController.prepend(::NoEmailExport::UsersControllerPatch)
   end
