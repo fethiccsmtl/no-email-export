618b27c9ef719e5a6861eb00eb9982c6c23b9d91
fethi bey abi ayad <fethi.bey.abi.ayad.ccsmtl@ssss.gouv.qc.ca>
2025-05-05 09:24:07 -0400

    fix exportCsvFiles

--- plugin.rb
+++ plugin.rb
@@ -1,31 +1,43 @@
 # name: no-email-export
-# about: Remove email from user CSV export
+# about: Removes email from user CSV export
 # version: 0.1
 # authors: Your Name
 
 after_initialize do
-    require_dependency 'admin/users_controller'
-  
     module ::NoEmailExport
-      module UsersControllerPatch
-        def export_csv
-          users = User.real.includes(:groups)
-          csv = CSV.generate do |csv|
-            csv << ["Username", "Name", "Created At", "Groups"]
-            users.find_each do |user|
-              csv << [
-                user.username,
-                user.name,
-                user.created_at,
-                user.groups.pluck(:name).join(", ")
-              ]
-            end
+      class RemoveEmailsFromExport < ::Jobs::ExportCsvFile
+        def execute(args)
+          return unless args[:type] == 'user_list'
+  
+          rows = []
+          headers = []
+  
+          User.real.includes(:groups).find_each do |user|
+            # Construis les données SANS l'email
+            row = {
+              "Username" => user.username,
+              "Name" => user.name,
+              "Created At" => user.created_at,
+              "Groups" => user.groups.pluck(:name).join(", ")
+            }
+            headers = row.keys if headers.empty?
+            rows << row
           end
   
-          send_data csv, type: 'text/csv; charset=utf-8; header=present', filename: "users.csv"
+          file_path = CsvExport.export(
+            headers,
+            rows,
+            "user_list"
+          )
+  
+          StaffExport.create!(
+            target_user_id: args[:current_user_id],
+            file: file_path,
+            status: StaffExport.statuses[:pending]
+          )
         end
       end
     end
   
-    ::Admin::UsersController.prepend(::NoEmailExport::UsersControllerPatch)
+    ::Jobs::ExportCsvFile.prepend(::NoEmailExport::RemoveEmailsFromExport)
   end
